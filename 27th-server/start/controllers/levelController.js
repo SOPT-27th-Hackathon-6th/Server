9c0c15aabf1c837b2390f94632bc8570f384b6c5
MyunDev <ygj01069@naver.com>
2020-11-22 00:05:41 +0900

    UPDATE: 레벨계산

--- 27th-server/start/controllers/levelController.js
+++ 27th-server/start/controllers/levelController.js
@@ -1,7 +1,7 @@
 const util = require('../utils/util');
 const responseMessage = require('../utils/responseMessage');
 const statusCode = require('../utils/statusCode');
-const { Level } = require('../models');
+const { Level, Mala, Rice } = require('../models');
 
 
 
@@ -13,8 +13,19 @@ module.exports = {
     getLevel: async(req, res) => {
         
         const foodType = Number.parseInt(req.params.foodType);
-        const levelNum = Number.parseInt(req.params.levelNum);
         
+        if (foodType === 0) {
+            //국밥인 경우
+            const riceData = await Rice.findAll()
+            const riceDatalength = riceData.length
+            var levelNum = riceDatalength / 3
+        } else {
+            //마라탕인 경우
+            const malaData = await Mala.findAll()
+            const malaDatalength = malaData.length
+            var levelNum = malaDatalength / 3
+        }
+        //const levelNum = Number.parseInt(req.params.levelNum);
         try {
             //SELECT * FROM LEVEL_TB WHERE foodType = foodType and levelNum = levelNum
             const data = await Level.findAll({
--- 27th-server/start/routes/level.js
+++ 27th-server/start/routes/level.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 
 
-router.get('/:foodType/:levelNum', levelController.getLevel)
+router.get('/:foodType', levelController.getLevel)
 
 module.exports = router;
