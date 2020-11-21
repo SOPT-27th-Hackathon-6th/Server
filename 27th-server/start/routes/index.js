92ec4d54dcd9ea2dcde56df126583141f2991dec
MyunDev <ygj01069@naver.com>
2020-11-21 19:50:42 +0900

    UPDATE: 줄 삭제

--- 27th-server/start/routes/index.js
+++ 27th-server/start/routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.use('/rice', require('./rice'))
-
 router.get("/", function (req, res, next) {
     res.render("index");
 });
