fcd2eee4fbb62a6a848d3d04a1c112877d74abab
MyunDev <ygj01069@naver.com>
2020-11-21 20:36:15 +0900

    UPDATE: 초키 세팅

--- 27th-server/start/routes/rice.js
+++ 27th-server/start/routes/rice.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const testController = require('../controllers/rice')
+const riceController = require('../controllers/rice')
 const router = express.Router();
 
 
 
-router.get('/', testController.getUser);
+router.get('/', riceController.getUser);
 
 
 module.exports = router;
